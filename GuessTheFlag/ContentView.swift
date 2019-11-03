81963b66a30ed9e2b7f835e1a146e381938ff1e5
Dennis Dang <dangggdennis@gmail.com>
2019-11-03 01:04:57 -0500

    Add opacity and rotation animation. No idea why the selected flag stays dimmed.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,11 +11,13 @@ import SwiftUI
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-    
+    @State private var angle: Double = 0
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var playerScore = 0
     @State private var selectedNumber = 0
+    @State private var correct = false
+    @State private var dim = 0.25
     
     var body: some View {
         ZStack {
@@ -34,8 +36,12 @@ struct ContentView: View {
                 ForEach(0 ..< 3) { number in
                     Button(action: {
                         self.flagTapped(number)
+                        self.angle = 360
                     }) {
                         FlagImage(country: self.countries[number])
+                            .opacity(number != self.selectedNumber && self.correct ? self.dim : 1)
+                            .rotation3DEffect(number == self.selectedNumber && self.correct ? .degrees(self.angle) : .degrees(0), axis: (x: 0, y: 1, z: 0))
+                            .animation(.default)
                     }
                 }
                 
@@ -50,11 +56,13 @@ struct ContentView: View {
             if scoreTitle == "Wrong" {
                 return Alert(title: Text("Wrong!"), message: Text("That's the flag of \(self.countries[self.selectedNumber])"), dismissButton: .destructive(Text("Continue")) {
                     self.askQuestion()
+                    self.resetRound()
                 })
             }
             
             return Alert(title: Text(scoreTitle), message: Text("Your score is \(self.playerScore)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
+                self.resetRound()
             })
         }
     }
@@ -63,17 +71,26 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             playerScore += 1
+            correct = true
         } else {
             scoreTitle = "Wrong"
         }
         
         selectedNumber = number
         showingScore = true
+        dim = 0.25
     }
     
     func askQuestion() {
         self.countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func resetRound() {
+        correct = false
+        angle = 0
+        selectedNumber = 4
+        dim = 1
     }
 }
 
